e68fc1cdc36fba737609b293e4953a023d662888
grahk93 <grace.juster@flatironschool.com>
2016-06-15 11:47:32 -0400

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,42 @@
+#Using other classes as objects? 
+
 class Transfer
-  # your code here
+
+  attr_accessor :sender, :receiver, :amount, :status
+
+  def initialize(sender,receiver,amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    if self.sender.valid? == true && self.receiver.valid? == true
+      return true
+    else
+      return false
+    end
+  end
+
+  def execute_transaction
+    if self.status != "complete"
+      self.sender.balance -= self.amount
+      self.receiver.balance += self.amount
+      self.status = "complete"
+    end #dont necessarily need an else #how to throw error 
+    if self.sender.valid? == false
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if self.status == "complete"
+      self.sender.balance += self.amount
+      self.receiver.balance -= self.amount
+      self.status = "reversed"
+    end
+  end
+
 end
